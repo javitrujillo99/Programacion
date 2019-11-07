6d821da499e12ff37a731887aae9cbd253c76e04
javitrujillo99 <43440645+javitrujillo99@users.noreply.github.com>
2019-11-07 13:40:30 +0100

    kk

---
+++ Desarrollo web entorno cliente_JavaScript/Ejercicios_JQuery/Ejercicio1/Ejercicio1.js
@@ -0,0 +1,8 @@
+function limpiar() {
+  /*  var usuario = document.getElementsByTagName("usuario");
+    var pass = document.getElementsByTagName("pass");
+    document.write(usuario);
+    document.write(pass);
+    */
+   document.write("Buenos dias");
+}
