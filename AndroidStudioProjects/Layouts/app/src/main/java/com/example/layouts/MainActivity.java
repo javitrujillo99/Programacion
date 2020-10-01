edcfdef9c96d65977ebda9e38eb7b335fa721aa3
javitrujillo99 <43440645+javitrujillo99@users.noreply.github.com>
2020-10-01 21:12:05 +0200

    Android

---
+++ AndroidStudioProjects/Layouts/app/src/main/java/com/example/layouts/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
